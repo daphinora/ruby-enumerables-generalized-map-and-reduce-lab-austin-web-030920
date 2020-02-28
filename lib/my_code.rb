5ef5e49c38372b58846c984b39f46dfd581793df
Jack Mitchell <jack.danielle.mitchell@gmail.com>
2020-02-28 19:19:17 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -8,7 +8,7 @@ def map(source_array)
   array
 end
 
-def reduce(source_array, starting_point = 0)
+def reduce(source_array, starting_point = nil)
   value = 0
   counter = 0
   while counter < source_array.length do
